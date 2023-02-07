e07b8f1de31a6c936a363cf76f6337c7da314c7f
Boris Chernov <chernov001@gmail.com>
2023-02-07 19:52:28 +0300

    Version 1

---
+++ Dockerfile
@@ -0,0 +1,60 @@
+#-----------------------------------------------------
+#
+#    This is Docker file of Wordpress and apache2
+#
+#    Date: 02-04-2023
+#
+#    Author: Boris Chernov chernov001@gmail.com
+#
+#-----------------------------------------------------
+
+FROM ubuntu:20.04
+
+#Installing nesseciary for Wordpress programs
+#MySQL we already have with database, user and privileges
+ENV TZ=UTC
+RUN apt-get -y update
+RUN apt-get install -y tzdata
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN apt-get -y install netcat
+RUN apt-get -y install telnet
+RUN apt-get -y install apache2
+RUN apt-get -y install ghostscript
+RUN apt-get -y install libapache2-mod-php
+RUN apt-get -y install php
+RUN apt-get -y install php-bcmath
+RUN apt-get -y install php-curl
+RUN apt-get -y install php-imagick
+RUN apt-get -y install php-intl
+RUN apt-get -y install php-json
+RUN apt-get -y install php-mbstring
+RUN apt-get -y install php-mysql
+RUN apt-get -y install php-xml
+RUN apt-get -y install php-zip
+RUN apt-get -y install wget
+RUN wget https://wordpress.org/latest.tar.gz
+RUN tar -zxvf latest.tar.gz
+RUN mv wordpress /var/www/
+RUN chown www-data:www-data /var/www/wordpress
+RUN chmod 755 /var/www/wordpress
+RUN echo "ServerName localhost" >> /etc/apache2/apache2.conf
+
+COPY wordpress.crt /etc/ssl/certs/wordpress.crt
+COPY wordpress.key /etc/ssl/private/wordpress.key
+#SSL key and cert for security connection of HTTP
+#RUN openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/wordpress.key -out /etc/ssl/certs/wordpress.crt
+
+#Copy wordpress config
+COPY wordpress.conf /etc/apache2/sites-available/
+COPY ssl-wordpress.conf /etc/apache2/sites-available/
+RUN a2ensite wordpress
+RUN a2enmod rewrite
+RUN a2dissite 000-default
+RUN a2ensite ssl-wordpress.conf
+RUN a2enmod ssl
+RUN service apache2 start
+COPY wp-config.php /var/www/wordpress/wp-config.php
+RUN service apache2 restart
+EXPOSE 80
+EXPOSE 443
+CMD ["apachectl", "-D", "FOREGROUND"]
---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** Database username */
+define( 'DB_USER', 'wordpress' );
+
+/** Database password */
+define( 'DB_PASSWORD', '0000' );
+
+/** Database hostname */
+define( 'DB_HOST', '192.168.31.242:3306' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'cJXIE4{pZcH}V:gRY7ptI@lg(2g[iR&6 .(f|zR{%q0MwbK`uK249w+ PmVA:mr1');
+define('SECURE_AUTH_KEY',  ':b]Ch+K}V}l+V]W-8FA|pIP=7PA9EI;`(.2J,DFR MjFc{RxQb4-=.n0ewY[*36*');
+define('LOGGED_IN_KEY',    '27{Sg8V-/seT|:2J|N)_>{SdbU9FB+=EaekTKP`nWd=ZQ5+Q+TziL=5f$+n|5/:V');
+define('NONCE_KEY',        '+CM~-{Nc2GC>|^^r*N+t~t^pgkKF6W2!7+:3ozBBmv%tsPjV*Smk8n-} HY^q[=?');
+define('AUTH_SALT',        '+C.>_88X:m5z9yWO<BCJYuOM9bj/;-gMg+<%ZhFQ+B,zFyF/|$,|w^Db]Jhm=P^-');
+define('SECURE_AUTH_SALT', 'w8ro1UNB?<[fPKhH@@dvl0L[k{tlxrEH|)o~n+`a!4-jP0DAl8}:sdK>Ir_l-2wk');
+define('LOGGED_IN_SALT',   '`-|%qkm%---o~4G~2~#XU5xi|7&em-hYaDd0hZb!.PyU+HyUpSvvcHrjR<u+QO{+');
+define('NONCE_SALT',       'qz|0!/M&67%v5zOnPZ-+aq2<-ZqO|#4Mh]tQ[jZ3NNY Z-N@[v2oj&DPHX6z`!R:');
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
